982277903b33e6cc1a77681f89be4b57ec2dd415
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-10-30 09:22:36 +0330

    js + some css ready

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const button = document.getElementById('toggle');
+const navBar = document.getElementById('nav');
+button.addEventListener('click', () => {
+  navBar.classList.toggle('active');
+});
